f71f525163045ee3a999fda59d83dc672a27ee2a
Ben Sloan <ben.sloan@ao.com>
2020-10-24 22:34:30 +0100

    Updated tests, adds different test cases.

--- PotterBookshop/PotterBookshopNetFramework/SetSplitterTests.cs
+++ PotterBookshop/PotterBookshopNetFramework/SetSplitterTests.cs
@@ -102,25 +102,31 @@ namespace PotterBookshopNetFramework
             Assert.AreEqual(2, result.Count);
         }
 
-        [Test]
-        public void WithBoundedSetSizeOf1_ThenThreeDifferentBooks_IsThreeDifferentSets()
+        [TestCase(1, 5)]
+        [TestCase(2, 3)]
+        [TestCase(3, 2)]
+        [TestCase(4, 2)]
+        [TestCase(5, 1)]
+        public void WithBoundedSetSizeOf1_ThenThreeDifferentBooks_IsThreeDifferentSets(int setSize, int expectedSplitCount)
         {
             var books = new List<Book>()
             {
                 new Book(1, "Book 1"),
                 new Book(2, "Book 2"),
-                new Book(3, "Book 3")
+                new Book(3, "Book 3"),
+                new Book(4, "Book 4"),
+                new Book(5, "Book 5")
             };
 
             var splitterOptions = new SetSplitterOptions
             {
-                MaximumSetSize = 1
+                MaximumSetSize = setSize
             };
 
             var splitter = new SetSplitter(splitterOptions);
             var result = splitter.Split(books);
 
-            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(expectedSplitCount, result.Count);
         }
     }
 
